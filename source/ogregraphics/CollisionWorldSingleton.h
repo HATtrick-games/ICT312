a6c8100d9ef7d9777bfc459ca2ca698420bebdf4
Arran <arran_ford@live.com>
2013-10-31 00:44:28 +0800

    More Doxygen

--- source/ogregraphics/CollisionWorldSingleton.h
+++ source/ogregraphics/CollisionWorldSingleton.h
@@ -15,21 +15,102 @@
 
 //#include "PhysicsEngine.h"
 
+/**
+ * \class	CollisionWorldSingleton
+ *
+ * \brief	Collision world singleton.
+ *
+ * \author	Arran Ford
+ * \date	31/10/2013
+ */
 
 class CollisionWorldSingleton
 {
 public:
+
+	/**
+	 * \fn	static CollisionWorldSingleton* CollisionWorldSingleton::Instance();
+	 *
+	 * \brief	Gets the instance.
+	 *
+	 * \author	Arran Ford
+	 * \date	31/10/2013
+	 *
+	 * \return	null if it fails, else.
+	 */
+
 	static CollisionWorldSingleton* Instance();
+
+	/**
+	 * \fn	void CollisionWorldSingleton::AddObject(btCollisionObject* colobj);
+	 *
+	 * \brief	Adds an object.
+	 *
+	 * \author	Arran Ford
+	 * \date	31/10/2013
+	 *
+	 * \param [in,out]	colobj	If non-null, the colobj.
+	 */
+
 	void AddObject(btCollisionObject* colobj);
+
+	/**
+	 * \fn	void CollisionWorldSingleton::CheckCollision();
+	 *
+	 * \brief	Check collision.
+	 *
+	 * \author	Arran Ford
+	 * \date	31/10/2013
+	 */
+
 	void CheckCollision();
 protected:
 private:
+
+	/**
+	 * \brief	Zero-based index of the.
+	 */
+
 	int i;
+
+	/**
+	 * \brief	The debug object.
+	 */
+
 	OgreBulletDraw* DebugObject;
+
+	/**
+	 * \brief	The broadphase.
+	 */
+
 	btDbvtBroadphase*	broadphase;
+
+	/**
+	 * \brief	The collision world.
+	 */
+
 	btCollisionWorld* collisionWorld;
+
+	/**
+	 * \brief	The dispatcher.
+	 */
+
 	btCollisionDispatcher* dispatcher;
+
+	/**
+	 * \brief	The collision configuration.
+	 */
+
 	btDefaultCollisionConfiguration* collisionConfiguration;
+
+	/**
+	 * \fn	CollisionWorldSingleton::CollisionWorldSingleton()
+	 *
+	 * \brief	Default constructor.
+	 *
+	 * \author	Arran Ford
+	 * \date	31/10/2013
+	 */
 
 	CollisionWorldSingleton()
 	{
@@ -44,5 +125,9 @@ private:
 		
 	}
 };
+
+/**
+// End of CollisionWorldSingleton.h
+ */
 
 #endif;
